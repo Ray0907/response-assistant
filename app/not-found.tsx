49905cbf6f68b648634943b5e006c048311724ff
Ray Tien <a0939552854@gmail.com>
2024-11-03 15:02:08 +0800

    Modify 404page.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -53,7 +53,7 @@ export default function NotFound() {
               Page Not Found
             </h2>
             <p className="text-gray-300 mb-6">
-              Oops! The page you're looking for doesn't exist.
+              Oops! The page you&apos;re looking for doesn&apos;t exist.
             </p>
             <Link
               href="/"
